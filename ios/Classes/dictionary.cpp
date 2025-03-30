e3536ea8123a91f9098a9a3509b5a3ab798ac825
‘kejun’ <kejuno@gmail.com>
2025-03-30 21:32:02 +0900

    更新项目以支持Flutter 3.29.x，修复Gradle和Kotlin版本兼容性问题。主要更改包括：更新 Kotlin 和 Gradle 版本，调整 Android SDK 版本，添加 C++ 标准设置，优化 .gitignore 文件以排除不必要的文件，删除过时的 Podfile.lock 文件，修复代码中的一些注册和调用方式，确保插件的正确集成。移除了所有C++代码中的register关键字,更新了过时的C++模板用法,在CMake配置中设置了C++14标准

--- ios/Classes/dictionary.cpp
+++ ios/Classes/dictionary.cpp
@@ -66,11 +66,10 @@ int progress_bar_darts(size_t current, size_t total) {
   return progress_bar("emitting double-array", current, total);
 }
 
-template <typename T1, typename T2>
-struct pair_1st_cmp: public std::binary_function<bool, T1, T2> {
-  bool operator()(const std::pair<T1, T2> &x1,
-                  const std::pair<T1, T2> &x2)  {
-    return x1.first < x2.first;
+template <class T1, class T2>
+struct pair_1st_cmp {
+  bool operator()(const std::pair<T1, T2>& x, const std::pair<T1, T2>& y) const {
+    return x.first < y.first;
   }
 };
 }  // namespace
